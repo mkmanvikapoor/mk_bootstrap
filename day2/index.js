6e44aedd6f2049dff626d44568ed2b476e3c329f
Manvi Kapoor <manvi.kapoor@shorthillstech.com>
2022-07-27 16:36:00 +0530

    day 2 assignment

---
+++ day2/index.js
@@ -0,0 +1,20 @@
+var data = [{
+    "name": "Manvi Kapoor",
+    "address": "delhi",
+    "phoneNumber": "999999999",
+    "previousJobType": "Internship",
+    "salaryExpectation": "30000 - 45000",
+    "maritalStatus": "Unmarried",
+    "jobLocation": "WFH",
+    "intro": "Axy vdf fdf fdsfsf dsf dsfdsfdsfds fdsfdsf"
+},
+{
+    "name": "Shwet Shikha",
+    "address": "delhi",
+    "phoneNumber": "999999999",
+    "previousJobType": "Internship",
+    "salaryExpectation": "30000 - 45000",
+    "maritalStatus": "Unmarried",
+    "jobLocation": "Remote",
+    "intro": "Axy vdf fdf fdsfsf dsf dsfdsfdsfds fdsfdsf"
+}];
